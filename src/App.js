2e03efa46999acf7c48165bbe894ec11343c58cc
raj bista <lomaslove132@gmail.com>
2020-04-16 11:37:17 +1000

    corona

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import { Cards, Chart, CountryPicker} from './components';
-import {fetchData} from './api';
-//import styles './App.module.css';
-import './App.module.css';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
 
 class App extends React.Component {
-  state ={
+  state = {
     data: {},
+    country: '',
   }
-  async componentDidMount () {
-    const fetchedData = await fetchData();
 
-  this.setState({data: fetchedData});
-     
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
   }
-  render (){
-  //const {data}= this.state;
-  return (
-     //<div className={styles.container}>
-    <div clasName="container"> 
-     <Cards data={this.state.data} />
-       <CountryPicker />
-       <Chart />
-    </div>
-  );
-}
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+       
+      
+      <div className={styles.container}>
+      <h1>Yubin Prisha Evani</h1>
+      <h2>Khashi Ayo Khashi Ko Samyuta Prustati</h2>
+        <img className={styles.image} src={image} alt="COVID-19"/>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
